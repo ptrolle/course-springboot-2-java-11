1391aeef633fb3c1f5e9534251b42f03fb2edd26
Pablo Trolle <pablotrolle@yahoo.com>
2022-09-22 22:13:18 -0300

    User entity and resource

---
+++ src/main/java/com/ptrolle/course/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.ptrolle.course.resources;
+
+import com.ptrolle.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping //para funcionar como um GET do http
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
