b7d0351ca00c3f1dc960f937c6ef2d8bda5958ce
Pablo Trolle <pablotrolle@yahoo.com>
2022-10-11 16:39:18 -0300

    Exception handling - Update

--- src/main/java/com/ptrolle/course/services/UserService.java
+++ src/main/java/com/ptrolle/course/services/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,9 +47,13 @@ public class UserService {
 
     //update
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     //deixando apenas atualizar, nome, email e telefone
